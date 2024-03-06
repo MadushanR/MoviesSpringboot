46f184d3b9b9c69dc636baf1b97448c2fd0d7648
MadushanR <77071289+MadushanR@users.noreply.github.com>
2024-03-06 00:15:17 -0500

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE movies_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255),
+    Genre VARCHAR(255),
+    Language VARCHAR(255),
+    Duration VARCHAR(255),
+    Price VARCHAR(255)
+);
